4f320725d358ac1b1130ebdb32a78bbb03c08f43
胡飞 <824166449@qq.com>
2022-02-27 22:05:22 +0800

    44

---
+++ src/component/Cropper/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import styles from "./styles.less";
+
+const Cropper = (): React.ReactElement => {
+
+  return <div className={styles.cropper}>
+
+  </div>;
+};
+
+export default Cropper;
